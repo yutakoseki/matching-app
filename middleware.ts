f8a720d9c5f94b71e5fa049835206e48ba858976
koseki <koseki@thinkingreed.co.jp>
2023-12-07 17:33:59 +0900

    auth周り修正middleware修正

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,8 @@
 export { default } from 'next-auth/middleware'; // defaultをママ使う。
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
+// 認証が必要なページ
 export const config = {
-    matcher: ['/((?!|api|login).*)'], // ?!で否定です。
+    matcher: ['/((?!welcome|api|login|signup).*)'], // ?!で否定です。
 };
